b0d763c231cb538f8229beb9df0690e99590b701
anton <antonzaritsky@gmail.com>
2022-04-18 21:17:54 +0200

    init commit

--- app/src/main/java/com/otus/homework/networkUtils/NetworkResult.kt
+++
@@ -1,8 +0,0 @@
-package com.otus.homework.networkUtils
-
-sealed class NetworkResult<out T : Any> {
-
-    data class Success<T : Any>(val body: T) : NetworkResult<T>()
-
-    data class Fail(val error: Throwable) : NetworkResult<Nothing>()
-}
