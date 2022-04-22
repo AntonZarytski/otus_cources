eb1ea20cde7c5fad15735c617e0affb52b9a47a3
anton <antonzaritsky@gmail.com>
2022-04-22 16:02:16 +0200

    finished 2st task

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class CatsViewModel(
     catsService: CatsService,
@@ -14,28 +17,30 @@ class CatsViewModel(
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private var requestDisposable: Disposable? = null
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+        requestDisposable =
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _catsLiveData.value = Success(it)
+                }, {
+                    _catsLiveData.value =
+                        Error(it.message ?: context.getString(R.string.default_error_text))
+                    throw it
+                })
     }
 
     fun getFacts() {}
+
+    override fun onCleared() {
+        if (requestDisposable != null && requestDisposable?.isDisposed!!) {
+            requestDisposable?.dispose()
+        }
+        super.onCleared()
+    }
 }
 
 class CatsViewModelFactory(
